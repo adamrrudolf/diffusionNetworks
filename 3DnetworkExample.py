6ab48a3ede177e751050b464b75b04f92f6dd8c9
Adam <adam.rybinski@outlook.com>
2015-04-12 21:31:54 +0200

    style changes

--- 3DnetworkExample.py
+++ 3DnetworkExample.py
@@ -3,21 +3,12 @@
 
 
 '''
-
-
-
+ 
 import dipy.viz.fvtk as fvtk
 import numpy as np
 
-
-
 label_coords = np.loadtxt('labels_coords_86.txt')
-
-
-
 labelsConnectivity = np.loadtxt('allconnectivityMatrix.txt')
-
-
 
 lines_color = [205/255.0,247/255.0,255/255.0]
 points_color = [2/255.0, 128/255.0, 232/255.0]
@@ -35,15 +26,13 @@ lineActors = fvtk.line(lines, lines_color, opacity=0.2, linewidth=2)
 
 
 fvtk.add(ren, pointActors)
-
 fvtk.add(ren, lineActors)
 
 
-#to explore the data in 3D interactive way
-#fvtk.show(ren)
+# to explore the data in 3D interactive way
+# fvtk.show(ren)
 
-
-#save figure
+# save figure
 
 fvtk.camera(ren, [-1, -1, 0], [0, 0, 0], viewup=[0, 0, 1])
 fvtk.record(ren, n_frames=1, 
